99c8a47f9916dae2c0fb5e7180925b3197dc3d20
noamalmog2004 <Noamalmog2004@gmail.com>
2022-12-08 09:30:57 +0200

    register page and validation functions in register class

--- app/src/main/java/com/example/buttonactivity/Register.java
+++ app/src/main/java/com/example/buttonactivity/Register.java
@@ -2,25 +2,45 @@ package com.example.buttonactivity;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class Register extends AppCompatActivity implements View.OnClickListener {
 
 
     Button btnRegister;
+    public EditText etPassword, etEmail, etFirstnanme, etAge;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
-        btnRegister = findViewById(R.id.btnRegister);
-        btnRegister.setOnClickListener(this);
+        //btnRegister = findViewById(R.id.btnRegister);
+        //btnRegister.setOnClickListener(this);
+        etAge = findViewById(R.id.etAge);
+        etPassword = findViewById(R.id.etPassword);
+        etFirstnanme = findViewById(R.id.etFirstname);
+        etEmail = findViewById(R.id.etEmail);
     }
-
-    @Override
     public void onClick(View view) {
         firebaseDB db = new firebaseDB();
-        db.register("secretpass", "noam@gmail.com");
+        String age = etAge.getText().toString();
+        String password = etPassword.getText().toString();
+        String email = etEmail.getText().toString();
+        String firstname = etFirstnanme.getText().toString();
+        if(!(db.firstnameValid(firstname)))
+            Toast.makeText(getApplicationContext(),"First name is not valid",Toast.LENGTH_SHORT).show();
+        else if(!(db.emailValid(email)))
+            Toast.makeText(getApplicationContext(),"Email is not valid",Toast.LENGTH_SHORT).show();
+        else if(!(db.ageValid(age)))
+            Toast.makeText(getApplicationContext(),"Age is not valid",Toast.LENGTH_SHORT).show();
+        else if(!(db.passwordValid(password)))
+            Toast.makeText(getApplicationContext(),"Password is not valid",Toast.LENGTH_SHORT).show();
+        else
+            db.register(password,email,age,firstname);
+
     }
 }
--- app/src/main/java/com/example/buttonactivity/firebaseDB.java
+++ app/src/main/java/com/example/buttonactivity/firebaseDB.java
@@ -1,10 +1,18 @@
 package com.example.buttonactivity;
 
+import android.view.View;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.firestore.FirebaseFirestore;
 
-public class firebaseDB {
 
+public class firebaseDB {
     private FirebaseAuth auth;
     private FirebaseFirestore fs;
     public firebaseDB()
@@ -12,9 +20,82 @@ public class firebaseDB {
         this.auth = FirebaseAuth.getInstance();
         this.fs = FirebaseFirestore.getInstance();
     }
-    public void register(String password, String email)
+    public void register(String password, String email, String age, String firstname)
     {
-        this.auth.createUserWithEmailAndPassword(email, password);
+        User user = new User(firstname, email, password, age);
+        this.auth.createUserWithEmailAndPassword(email, password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+
+            }
+        });
+        this.fs.collection("users").document(email).set(user);
+    }
+    class User
+    {
+        public String firstname;
+        public String password;
+        public String email;
+        public String age;
+        User( String firstname,  String email,  String password, String age)
+        {
+            this.age = age;
+            this.firstname = firstname;
+            this.email = email;
+            this.password = password;
+        }
+    }
+    public boolean firstnameValid(String firstname)
+    {
+        if (firstname.length() == 0) {
+            return false;
+        }
+        for (int i = 0; i < firstname.length(); i++) {
+            if (!(firstname.charAt(i) > 'a' && firstname.charAt(i) < 'z' || firstname.charAt(i) > 'A' && firstname.charAt(i) < 'Z')) {
+                return false;
+            }
+        }
+        return true;
+    }
+    public  boolean emailValid(String email)
+    {
+        String checkEmail = "[a-zA-Z0-9._-]+@[a-z]+.+[a-z]+";
+        if (email.isEmpty())
+            return false;
+        else if (!email.matches(checkEmail))
+            return false;
+        return true;
     }
 
+    public boolean passwordValid(String password)
+    {
+        String checkPassword = "^" +
+                "(?=.*[0-9])" +         //at least 1 digit
+                "(?=.*[a-z])" +         //at least 1 lower case letter
+                "(?=.*[A-Z])" +         //at least 1 upper case letter
+                "(?=.*[a-zA-Z])" +      //any letter
+                "(?=.*[@#$%^&+=])" +    //at least 1 special character
+                "(?=S+$)" +           //no white spaces
+                ".{4,}" +               //at least 4 characters
+                "$";
+        if (password.isEmpty())
+            return false;
+        else if (!password.matches(checkPassword))
+            return false;
+        else
+            return true;
+    }
+    public boolean ageValid(String age)
+    {
+        //IF I == 2, AGE IS 100+
+        if (age.length()>2)
+            return false;
+
+        for (int i = 0; i < age.length(); i++)
+        {
+            if(!(age.charAt(i)>'0' && age.charAt(i)<'9'))
+                return false;
+        }
+        return true;
+    }
 }
